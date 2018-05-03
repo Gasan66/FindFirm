0f93b092c14744e8f17889e7b19b9933306d460a
gasan <abduragimov.ga@gmail.com>
2018-05-03 17:45:30 +0500

    1

--- venv/FindFirm.py
+++ venv/FindFirm.py
@@ -11,7 +11,8 @@ url_inn = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party'
 headers = {'Authorization': 'Token 69df660da2b7ae419968a6c8ffe4108a59704d84'
            , 'Content-Type': 'application/json'
            , 'Accept': 'application/json'}
-data = {'query': '6686100430'}
+data = {'query': '6686100430'
+        , 'branch_type': 'MAIN'}
 # params_inn = {'Authorization': 'Token 69df660da2b7ae419968a6c8ffe4108a59704d84'
 #               , 'query': '6686100430'}
 
@@ -20,29 +21,30 @@ res_inn = json.loads(req_inn.text)
 print(res_inn)
 
 
-# url_loc = 'https://search-maps.yandex.ru/v1/'
-# params_loc = {'apikey': 'a7478553-952d-42ca-8264-b80a07ae6341'
-#           , 'text': 'Пивко, Нягань'
-#           , 'type': 'biz'
-#           , 'lang': 'ru-RU'
-#           , 'results': '500'}
-#
-# # Запрос на яндекс
-# req = requests.get(url_loc, params=params_loc)
-#
-# # Сохраняем овет
-# res = json.loads(req.text)
-#
-# # Получаем список организаций
-# list_org = res.get('features')
-#
-#
-# for org in list_org:
-#     # k +=1
-#     print(org.get('properties').get('CompanyMetaData').get('name')
-#           , org.get('properties').get('CompanyMetaData').get('address')
-#           , org.get('properties').get('CompanyMetaData').get('Categories')[0].get('name')
-#           , org.get('properties').get('CompanyMetaData').get('Phones')[0].get('formatted')
-#           , org.get('properties').get('CompanyMetaData').get('Hours').get('text'))
-# print(org.get('properties').get('CompanyMetaData').keys())
-# print(org.get('properties').get('CompanyMetaData').values())
+url_loc = 'https://search-maps.yandex.ru/v1/'
+params_loc = {'apikey': 'a7478553-952d-42ca-8264-b80a07ae6341'
+          , 'text': 'Пивко, Нягань'
+          , 'type': 'biz'
+          , 'lang': 'ru-RU'
+          , 'results': '500'}
+
+# Запрос на яндекс
+req = requests.get(url_loc, params=params_loc)
+
+# Сохраняем овет
+res = json.loads(req.text)
+print(req.text)
+
+# Получаем список организаций
+list_org = res.get('features')
+
+
+for org in list_org:
+    # k +=1
+    print(org.get('properties').get('CompanyMetaData').get('name')
+          , org.get('properties').get('CompanyMetaData').get('address')
+          , org.get('properties').get('CompanyMetaData').get('Categories')[0].get('name')
+          , org.get('properties').get('CompanyMetaData').get('Phones')[0].get('formatted')
+          , org.get('properties').get('CompanyMetaData').get('Hours').get('text'))
+print(org.get('properties').get('CompanyMetaData').keys())
+print(org.get('properties').get('CompanyMetaData').values())
